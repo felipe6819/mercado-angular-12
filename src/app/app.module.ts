74a7b70eb790c5e754b3e41a329af6be29e96b61
felipe6819 <128556670+felipe6819@users.noreply.github.com>
2023-09-30 12:17:52 -0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgbModule } from 'ngx-bootstrap';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CaixaComponent } from '../caixa/caixa.component';
@@ -7,7 +6,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, CaixaComponent],
-  imports: [BrowserModule, ReactiveFormsModule, FormsModule, NgbModule],
+  imports: [BrowserModule, ReactiveFormsModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
