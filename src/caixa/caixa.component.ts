fc6ca04fb805c1e162dd2d0ebd0d053ac1e9be6d
felipe6819 <128556670+felipe6819@users.noreply.github.com>
2023-09-30 11:39:52 -0300

    3 files modified, 3 added

---
+++ src/caixa/caixa.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+type Produtos = {
+  nome: string;
+  preco: number;
+  codigo: string;
+};
+
+@Component({
+  selector: 'app-caixa',
+  templateUrl: './caixa.component.html',
+  styleUrls: ['./caixa.component.css'],
+})
+export class CaixaComponent implements OnInit {
+  resource: FormGroup;
+  produtos: Produtos[] = [];
+
+  produtosCadastrados: Produtos[] = [];
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.resource = this.fb.group({
+      codigo: [null, [Validators.required]],
+    });
+  }
+
+  cadastrarProduto() {
+    if (this.resource.invalid) this.resource.markAllAsTouched();
+    const codigo = this.resource.get('codigo').value;
+    const produto = this.produtos.find((x) => x.codigo === codigo);
+
+    if (produto) this.produtosCadastrados.push(produto);
+    this.resource.reset();
+  }
+}
