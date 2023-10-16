34686dd61649b765dfe9a13349e06f511faae9e8
felipe6819 <128556670+felipe6819@users.noreply.github.com>
2023-10-16 20:34:27 -0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,5 @@ export class AppComponent {
   title: string = 'MARKETPLACE';
 
   constructor(private meuServico: MeuServicoService) {
-    // O serviço está injetado e disponível para uso aqui.
-  }
+ }
 }
